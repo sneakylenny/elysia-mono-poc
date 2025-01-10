326c2a1d0e3c1d75d57cae0be091feae3c7e2c64
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2025-01-10 09:53:18 +0100

    Use pages

--- packages/client/app/app.vue
+++ packages/client/app/app.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
-import { treaty } from '@elysiajs/eden';
-import type { Server } from '@my-monorepo/server'
-
-const client = treaty<Server>('localhost:3000/api/server')
-
-const result = ref(0)
-
-onMounted(() => {
-  const liveRng = client.rng.subscribe()
-
-  liveRng.subscribe((message) => {
-    result.value = message.data.randomNumber
-  })
-})
+//
 </script>
 
 <template>
-  <div>
-    Hello Nuxt
-
-    <div>
-      <pre v-text="result"></pre>
-    </div>
-  </div>
+  <NuxtLoadingIndicator />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
